6614067765337ee6b3e54336fdaabe28cddf91c6
Leonardo Pêgo Martins <leonardopegomartins@gmail.com>
2020-06-15 16:18:48 -0300

    Melhorando as telas

--- app/src/main/java/com/example/primeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.primeiroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Chamando a outra tela pelo botao.
+        val btnReaisPontos = findViewById<Button>(R.id.btnPontosReais)
+            btnReaisPontos.setOnClickListener{
+                val intent = Intent(this, Pontos_Reais::class.java)
+                startActivity(intent)
+
+                
+            }
     }
 }
+
---
+++ app/src/main/java/com/example/primeiroapp/Pontos_Reais.kt
@@ -0,0 +1,11 @@
+package com.example.primeiroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pontos_Reais : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pontos__reais)
+    }
+}
